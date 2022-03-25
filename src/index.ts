453e8a7a80ed820fb7f0d89bcc51a304a83f35e5
amandastecz <amandasstecz@gmail.com>
2022-03-24 21:03:13 -0300

    docs: added api documentation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
-const app = express();
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
+const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
